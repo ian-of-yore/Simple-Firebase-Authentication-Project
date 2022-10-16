2c152fe718825211eebbf3b39ace555d1a2bf748
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-10-16 13:50:00 +0600

    Basic Project Setup with routes done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import LogIn from './components/LogIn';
+import Register from './components/Register';
+import Main from './layout/Main';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/login',
+          element: <LogIn></LogIn>
+        },
+        {
+          path: '/register',
+          element: <Register></Register>
+        }
+      ]
+    }
+  ])
+
+
   return (
     <div className="App">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/LogIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogIn = () => {
+    return (
+        <div>
+            <h2>This is LogIn Component</h2>
+        </div>
+    );
+};
+
+export default LogIn;
